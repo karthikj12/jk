1dceb1a0525ab362530788ea08b76c84b29197af
karthikj12 <54841903+karthikj12@users.noreply.github.com>
2019-09-05 15:15:05 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World from Jenkins");
+		System.out.println("Hello World from Github");
 	}
 }
 // added comment after add cmd at git
